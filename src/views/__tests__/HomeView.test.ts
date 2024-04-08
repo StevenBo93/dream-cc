993fd87d1b7841fbfbb259359d829a129088a3a3
StevenBo93 <stevenbo93@gmail.com>
2024-04-08 13:19:38 +0200

    adjust test

--- src/views/__tests__/HomeView.test.ts
+++ src/views/__tests__/HomeView.test.ts
@@ -1,26 +1,25 @@
-import { describe, it, expect, beforeEach } from 'vitest';
-import { mount } from '@vue/test-utils';
-import { createPinia, setActivePinia } from 'pinia';
-import HomeView from '@/views/HomeView.vue';
-import CategoryComponent from '@/components/ui/CategoryComponent.vue';
-import router from '@/router';
+import { describe, it, expect, beforeEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createPinia, setActivePinia } from 'pinia'
+import HomeView from '@/views/HomeView.vue'
+import CategoryComponent from '@/components/ui/CategoryComponent.vue'
+import router from '@/router'
 
 describe('HomeView', () => {
   beforeEach(() => {
-    setActivePinia(createPinia());
-  });
+    setActivePinia(createPinia())
+  })
 
   it('correctly loads and renders CategoryComponent', async () => {
-
     const wrapper = mount(HomeView, {
       global: {
-        plugins: [router, createPinia()],
-      },
-    });
+        plugins: [router, createPinia()]
+      }
+    })
 
-    await router.isReady();
+    await router.isReady()
 
-    expect(wrapper.exists()).toBe(true);
-    expect(wrapper.findComponent(CategoryComponent).exists()).toBe(true);
-  });
-});
+    expect(wrapper.exists()).equals(true)
+    expect(wrapper.findComponent(CategoryComponent).exists()).equals(true)
+  })
+})
